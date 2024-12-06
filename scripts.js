49228a818617ff90e7e1eee0f341f0294ac6d858
lib <acopomm@gmail.com>
2024-12-06 13:48:25 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 function tymsg() { 
-    document.getElementById("thanku").style.opacity = "100";
-    setTimeout(() => {
-      document.getElementById("thanku").style.opacity = "0"
-    }, 1000)
-  }
-
+  document.getElementById("thanku").style.opacity = "100";
+  document.body.style.backgroundImage = 'linear-gradient(rgba(145, 250, 162,0) 85%, rgba(145, 250, 162,1))';
+  setTimeout(() => {
+    document.getElementById("thanku").style.opacity = "0";
+    document.body.style.backgroundImage = 'linear-gradient(rgba(145, 250, 162,0) 85%, rgba(145, 250, 162,0))';
+  }, 1000)
+}
 function rate(a) {
   var d = new Date();
   var day = d.getDate();
